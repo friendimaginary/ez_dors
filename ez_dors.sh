0edfdc462eee5fd2d4306162d5948c2f905c0d17
Patryk Soika <patryk@superrenaissanceman.com>
2017-08-12 12:28:05 -0400

    First draft of script that's ready to test. Let's see what does or does not go wrong...

--- ez_dors.sh
+++ ez_dors.sh
@@ -1,5 +1,51 @@
 #! /bin/bash
 
+echo -e "\n\n\n"
+echo "Hello. I am about to configure RStudio Server for you, on what  I assume "
+echo "is an Ubuntu 16.04 LTS droplet running in Digital Ocean. This script may "
+echo "work in other circumstances, but that is how it was written and tested. "
+
+wait 2
+
+echo -e "\n\n\nFor the purposes of configuring git, can I get your name?"
+echo -e "You can leave it blank or say 'Jon Snow' if you don't trust me.\n"
+read -p "Name:" git_name
+
+echo read -p "Please enter the number to validate :" mynum
+echo -e "\nFor the purposes of configuring git, can I get your email address?"
+echo -e "You can leave it blank or say 'snow@thewall.not_a_real.tld if you don't trust me.\n"
+read -p "Email:" git_email
+
+
+if [ expr length "$(echo $git_email)" < 1 ]
+  then git_email = "snow@thewall.not_a_real.tld"
+fi
+
+if [expr length "$(echo $git_name)" < 1 ]
+  then git_name = "Jon Snow"
+fi
+
+git config --global user.name  $git_name
+git config --global user.email $git_email
+
+echo -e "\n\n\nThanks. Here is your git config:"
+echo ""
+cat .gitconfig
+echo -e "\n\n\n"
+
+wait 2
+echo -e "Now, pick a username and password to use for RStudio.\n"
+
+read -p "username:" rstudio_user
+read -p "password:" rstudio_pass
+
+useradd -m $rstudio_user
+echo "$rstudio_user:$rstudio_pass" | chpasswd
+
+echo -e "\n\nThat should do it.\n\nNow we're ready to install and update packages.\n\n"
+
+read -p "Press key to continue.. " -n1 -s
+
 dpkg --configure -a
 apt update
 apt -y full-upgrade -f
@@ -21,6 +67,17 @@ md5sum rstudio-server-1.0.153-amd64.deb| grep b8df8478e446851dbe0ce893d32e3e67
 echo -e "\n\n\nIf the md5 doesn't match, abort with Ctrl-C. Otherwise:\n\n"
 read -p "Press key to continue.. " -n1 -s
 
-gdebi rstudio-server-1.0.153-amd64.deb
+gdebi --n rstudio-server-1.0.153-amd64.deb
+
+dpkg --configure -a
+apt update
+apt -y full-upgrade -f
+apt -y autoremove
+
+echo -e "\n\n\nYou should now be able to login to the Rstudio Server by point a web browser at your IP address."
+echo -e "\nHere's an example:\n\thttp://123.45.67.89:8787."
+echo -e "\nHere's your IP addres info, as well:"
+curl ipinfo.io
+echo -e "\n\n\n"
 
 
